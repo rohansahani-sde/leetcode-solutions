ec6292f8437a2e026408bdf62f9bcb4903a2900c
Rohan sahani <sahanirohan313@gmail.com>
2026-04-01 09:24:29 +0530

    Easy: 3880. Minimum Absolute Difference Between Two Values

--- 3880. Minimum Absolute Difference Between Two Values/3880. Minimum Absolute Difference Between Two Values.java
+++ 3880. Minimum Absolute Difference Between Two Values/3880. Minimum Absolute Difference Between Two Values.java
@@ -1,5 +1,18 @@
 class Solution {
     public int minAbsoluteDifference(int[] nums) {
-        
+        int ans =101;
+        int n = nums.length;
+        for(int i=0; i<n; i++){
+            int x = nums[i];
+            if(x == 1){
+                for(int j=0; j<n; j++){
+                    int y= nums[j];
+                    if(y ==2){
+                        ans = Math.min(ans, Math.abs(j-i));
+                    }
+                }
+            }
+        }
+        return ans != 101 ? ans : -1; 
     }
 }
