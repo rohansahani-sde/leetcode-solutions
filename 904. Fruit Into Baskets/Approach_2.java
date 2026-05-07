ebb33b4e8d0722139a9ff97c2bdddbb2f75b2156
Rohan sahani <sahanirohan313@gmail.com>
2026-05-07 08:50:58 +0530

    Medium: 904. Fruit Into Baskets| 53 ms | 70.4 MB

---
+++ 904. Fruit Into Baskets/Approach_2.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int totalFruit(int[] fruits) {
+        Map<Integer, Integer> map =new HashMap<>();
+        int j=0;
+        int n = fruits.length;
+        int ans=0;
+        
+        for(int i=0; i<n; i++){
+            map.put(fruits[i], map.getOrDefault(fruits[i], 0)+1);
+            
+            while(map.size() > 2){
+                map.put(fruits[j], map.get(fruits[j])-1);
+                if(map.get(fruits[j]) == 0) map.remove(fruits[j]);
+                j++;
+            }
+            ans = Math.max(ans, i-j+1);
+
+        }
+        return ans;
+    }
+}
