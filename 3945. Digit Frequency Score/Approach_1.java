3afd39488af872e41ff08b98895d4dc529a60f19
Rohan sahani <sahanirohan313@gmail.com>
2026-06-01 11:14:51 +0530

    Easy: 3945. Digit Frequency Score| 1 ms | 42.5 MB

---
+++ 3945. Digit Frequency Score/Approach_1.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int digitFrequencyScore(int n) {
+        int[] arr = new int[10];
+        while(n > 0){
+            int sd = n %10;
+            arr[sd]++;
+            n /= 10;
+        }
+        int ans=0;
+        for(int i=0; i<10; i++){
+            ans += (i * arr[i]);
+        }
+        return ans;
+    }
+}
