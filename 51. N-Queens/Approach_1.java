9d1cef66556c3221f12ad4b2ecc5a92162323c5a
Rohan sahani <sahanirohan313@gmail.com>
2026-06-18 08:32:28 +0530

    Hard: 51. N-Queens | Approach 1 | Runtime: 3 | Memory: 46856000

---
+++ 51. N-Queens/Approach_1.java
@@ -0,0 +1,56 @@
+class Solution {
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> ans = new ArrayList<>(); 
+        char[][] mat = new char[n][n];
+        for(char[] ma: mat){
+            Arrays.fill(ma, '.');
+        }
+        fun(mat, 0, n, ans);
+        
+        return ans;
+        
+    }
+    private void fun(char[][] mat, int row, int n, List<List<String>> ans ){
+        if(row == n){
+            List<String> l = new ArrayList<>();
+
+            for(char[] ma: mat){
+                String s = new String(ma);
+                l.add(s);
+            }
+            ans.add(new ArrayList<>(l));
+            return;
+        }
+
+        for(int j=0; j<n; j++){
+            if(isValidPlace(mat, row, j, n)){
+            mat[row][j] = 'Q';
+            fun(mat, row+1, n, ans);
+            mat[row][j] ='.';
+            } 
+             
+        }
+    }
+    private boolean isValidPlace(char[][] mat, int row, int col, int n) {
+    for (int i = 0; i < row; i++) {
+        if (mat[i][col] == 'Q') return false;
+    }
+
+    int r = row, c = col;
+    while (r >= 0 && c >= 0) {
+        if (mat[r][c] == 'Q') return false;
+        r--;
+        c--;
+    }
+
+    r = row;
+    c = col;
+    while (r >= 0 && c < n) {
+        if (mat[r][c] == 'Q') return false;
+        r--;
+        c++;
+    }
+
+    return true;
+}
+}
