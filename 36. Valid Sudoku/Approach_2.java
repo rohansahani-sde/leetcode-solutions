6e877f646e600245c2708984a37d8b2c6a69e9de
Rohan sahani <sahanirohan313@gmail.com>
2026-06-18 10:26:31 +0530

    Medium: 36. Valid Sudoku | Approach 2 | Runtime: 3 | Memory: 46376000

---
+++ 36. Valid Sudoku/Approach_2.java
@@ -0,0 +1,54 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        // all rows
+        for(int i=0; i<9;i++){
+            HashSet<Character> setRow = new HashSet<>();
+            HashSet<Character> setCol = new HashSet<>();
+                
+            for(int j=0; j<9; j++){
+                if(board[i][j] != '.'){
+                    if(setRow.contains(board[i][j])) return false;
+                    setRow.add(board[i][j]);
+                }
+                if(board[j][i] != '.'){
+                    if(setCol.contains(board[j][i])) return false;
+                    setCol.add(board[j][i]);
+                }
+                
+            }
+        }
+        // all cols
+        // for(int j=0; j<9;j++){
+        //     HashSet<Character> set = new HashSet<>();
+        //     for(int i=0; i<9; i++){
+        //         if(board[i][j] =='.')continue;
+        //         if(set.contains(board[i][j])) return false;
+        //         set.add(board[i][j]);
+        //     }
+        // }
+        // all boxes
+        for(int i=0; i<9; i+=3){
+            int endI = i+2;
+            for(int j=0; j<9; j+=3){
+                int endJ = j+2;
+                if(!traverseBox(i, endI, j, endJ, board)) return false;
+                
+            }
+
+        }
+
+        return true;
+
+    }
+    private boolean traverseBox(int si, int ei, int sj, int ej, char[][] mat){
+        HashSet<Character> set = new HashSet<>();
+        for(int i=si; i<=ei; i++){
+            for(int j=sj; j<=ej; j++){
+                if(mat[i][j] == '.')continue;
+                if(set.contains(mat[i][j])) return false;
+                set.add(mat[i][j]);
+            }
+        }
+        return true;
+    }
+}
