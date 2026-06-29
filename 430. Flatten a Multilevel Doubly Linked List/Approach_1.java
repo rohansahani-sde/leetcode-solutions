7dedd61fdd724b56462c9d40598fb3a6ceb7cc44
Rohan sahani <sahanirohan313@gmail.com>
2026-06-29 09:08:24 +0530

    Medium: 430. Flatten a Multilevel Doubly Linked List | Approach 1 | Runtime: 0 ms | Memory: 43340000

---
+++ 430. Flatten a Multilevel Doubly Linked List/Approach_1.java
@@ -0,0 +1,33 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node prev;
+    public Node next;
+    public Node child;
+};
+*/
+
+class Solution {
+    public Node flatten(Node head) {
+        Node curr  = head;
+        while(curr != null){
+            if(curr.child != null){
+                Node nextNode = curr.next;
+                Node childTail = curr.child;
+                while(childTail.next != null) childTail = childTail.next;
+                if(nextNode != null){
+                    childTail.next = nextNode;
+                    nextNode.prev = childTail;
+                }
+
+                curr.next = curr.child;
+                curr.child.prev = curr;
+                curr.child =null; 
+
+            }
+            curr = curr.next;
+        }
+        return head;
+    }
+}
