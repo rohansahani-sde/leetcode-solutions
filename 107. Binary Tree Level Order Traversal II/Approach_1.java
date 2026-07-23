62f0fab8d984620cf973cb1c67e22069e5393359
Rohan sahani <sahanirohan313@gmail.com>
2026-07-23 10:19:40 +0530

    Medium: 107. Binary Tree Level Order Traversal II | Approach 1 | Runtime: 1 | Memory: 44400000

---
+++ 107. Binary Tree Level Order Traversal II/Approach_1.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<>();
+        if(root == null) return ans;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        while(!q.isEmpty()){
+            int size = q.size();
+            List<Integer> l =new ArrayList<>();
+            while(size-- >0){
+                TreeNode node = q.poll();
+                if(node == null) continue;
+                l.add(node.val);
+                if(node.left != null) q.offer(node.left);
+                if(node.right != null) q.offer(node.right);
+            }
+            ans.add(0, l);
+        }
+        return ans;
+    }
+}
